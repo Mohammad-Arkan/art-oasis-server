76d436327319e76b1bd33c2878b0ac2bc3cbf669
Zihadul Islam <zynscriptofficial@gmail.com>
2023-06-29 23:13:34 +0600

    jwt token verify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const {MongoClient, ServerApiVersion} = require("mongodb");
@@ -8,6 +9,24 @@ const {MongoClient, ServerApiVersion} = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message: "Unauthorized Access"});
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({error: true, message: "Unauthorized Access"});
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.ll4tlqm.mongodb.net/?retryWrites=true&w=majority`;
 
