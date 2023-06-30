89d7e966183006ecc6a9692667659d9b6fb6df69
Zihadul Islam <zynscriptofficial@gmail.com>
2023-06-30 19:25:07 +0600

    verify admin

--- index.js
+++ index.js
@@ -65,7 +65,18 @@ async function run() {
       next();
     };
 
-    app.get("/users", verifyJWT, async (req, res) => {
+    // admin verify
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({error: true, message: "Forbidden Access"});
+      }
+      next();
+    };
+
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
